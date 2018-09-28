7e6ac433bce58e8f0ae3d5e6cdaf500a9ac6b291
CAITI <tim.cai@oocl.com>
2018-09-28 02:21:23 -0700

    change port from 5000 to 8080

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello World!"
+    return "Tim's website by flask on Openshift!"
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', port=8080)
